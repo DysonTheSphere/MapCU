3cccaa40a78ae00a40e62353b543ded619c545de
Phalgun4 <57873193+Phalgun4@users.noreply.github.com>
2020-03-19 16:17:38 -0600

    Add files via upload

---
+++ Final Project CSCI 3308/FinalProject_Home.js
@@ -0,0 +1,6 @@
+function onClickFunction() {
+  document.getElementById("search_id").onclick = function ()
+  {
+      location.href = "file:///Users/phalguntaman/Desktop/Final%20Project%20CSCI%203308/navigation.html";
+  };
+}
